350249f10b5cc2b71a5ced8073d34391eb113972
erdenezul ganbat <semjidtv@gmail.com>
2026-08-08 17:15:06 +0800

    Update root directory paths and app logic

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const path = require('path');
-const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
@@ -13,22 +12,20 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Файлын замыг боломжит бүх байршлаар шалгах
-const rootPublic = path.join(__dirname, '../public');
-const serverPublic = path.join(__dirname, 'public');
-
-if (fs.existsSync(rootPublic)) {
-  app.use(express.static(rootPublic));
-}
-if (fs.existsSync(serverPublic)) {
-  app.use(express.static(serverPublic));
-}
+// public хавтасны файлуудыг ачаалах
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB холболт
 const MONGODB_URI = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI)
-  .then(() => console.log('MongoDB холбогдлоо'))
-  .catch(err => console.error('MongoDB холболтын алдаа:', err));
+
+if (!MONGODB_URI) {
+  console.error("MONGODB_URI Environment Variable тохируулагдаагүй байна!");
+} else {
+  mongoose.connect(MONGODB_URI)
+    .then(() => console.log('MongoDB амжилттай холбогдлоо'))
+    .catch(err => console.error('MongoDB холболтын алдаа:', err));
+}
 
 // Socket.io холболт
 io.on('connection', (socket) => {
@@ -39,19 +36,14 @@ io.on('connection', (socket) => {
   });
 });
 
-// Үндсэн хуудсыг автоматаар хайж буцаах
+// Үндсэн хуудсыг буцаах
 app.get('*', (req, res) => {
-  const file1 = path.join(rootPublic, 'index.html');
-  const file2 = path.join(serverPublic, 'index.html');
-
-  if (fs.existsSync(file1)) {
-    res.sendFile(file1);
-  } else if (fs.existsSync(file2)) {
-    res.sendFile(file2);
-  } else {
-    res.status(404).send('index.html файл олдсонгүй. Хавтасны бүтцээ шалгана уу.');
-  }
+  res.sendFile(path.join(__dirname, '../public/index.html'), (err) => {
+    if (err) {
+      res.sendFile(path.join(__dirname, 'public/index.html'));
+    }
+  });
 });
 
 const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`Сервер ${PORT} дээр ажиллаж байна`));
+server.listen(PORT, () => console.log(`Сервер ${PORT} порт дээр ажиллаж байна.`));
