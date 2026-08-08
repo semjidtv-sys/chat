e69ae49294870aabc6e20360f086b42a6f9b46ea
erdenezul ganbat <semjidtv@gmail.com>
2026-08-08 17:16:30 +0800

    Auto detect index.html path

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
@@ -12,16 +13,14 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// public хавтасны файлуудыг ачаалах
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, 'public')));
+// Боломжит бүх хавтаснаас CSS, JS, Зургуудыг ачаалах
+app.use(express.static(path.join(__dirname, '..')));              // Root хавтас
+app.use(express.static(path.join(__dirname, '../public')));       // Root/public хавтас
+app.use(express.static(path.join(__dirname, 'public')));          // Server/public хавтас
 
 // MongoDB холболт
 const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  console.error("MONGODB_URI Environment Variable тохируулагдаагүй байна!");
-} else {
+if (MONGODB_URI) {
   mongoose.connect(MONGODB_URI)
     .then(() => console.log('MongoDB амжилттай холбогдлоо'))
     .catch(err => console.error('MongoDB холболтын алдаа:', err));
@@ -36,13 +35,22 @@ io.on('connection', (socket) => {
   });
 });
 
-// Үндсэн хуудсыг буцаах
+// index.html файлыг бүх боломжит байршлаас хайж буцаах
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'), (err) => {
-    if (err) {
-      res.sendFile(path.join(__dirname, 'public/index.html'));
+  const possiblePaths = [
+    path.join(__dirname, '../index.html'),        // Root дотор байгаа бол
+    path.join(__dirname, '../public/index.html'), // Root/public дотор байгаа бол
+    path.join(__dirname, 'public/index.html'),    // Server/public дотор байгаа бол
+    path.join(__dirname, 'index.html')            // Server дотор байгаа бол
+  ];
+
+  for (const filePath of possiblePaths) {
+    if (fs.existsSync(filePath)) {
+      return res.sendFile(filePath);
     }
-  });
+  }
+
+  res.status(404).send('index.html файл бүх байршилд олдсонгүй.');
 });
 
 const PORT = process.env.PORT || 10000;
